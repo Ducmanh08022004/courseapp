88ee07ccbbe619de3881404b13ed6b95ebf9150f
Ducmanh08022004 <mt0942511@gmail.com>
2025-10-20 10:44:59 +0700

    update getCourseByTitle

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.get('/:title', async (req, res) => {
+router.get('/title/:title', async (req, res) => {
   try {
     const course = await Course.findOne({ where: { title: req.params.title } });
     if (!course) return res.status(404).json({ msg: 'Course not found' });
